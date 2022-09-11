a0a804383a27a72ae33fd8e8172f66357a903097
Chris Cresswell <Christorper@gmail.com>
2022-09-11 00:36:37 -0700

    Set up queue and breadth first search

--- knightMoves.js
+++ knightMoves.js
@@ -1,62 +1,61 @@
-const knightMoves = () => {
-  const makeBoard = () => {
-    const board = [];
-    for (let i = 0; i < 8; i++) {
-      board[i] = [];
-      for (let j = 0; j < 8; j++) {
-        board[i][j] = `[${i}, ${j}]`;
-      }
+const makeBoard = () => {
+  const board = [];
+  for (let i = 0; i < 8; i++) {
+    board[i] = [];
+    for (let j = 0; j < 8; j++) {
+      board[i][j] = `[${i}, ${j}]`;
     }
-    return board;
-  };
-
-  const exists = (x, y) => {
-    const board = makeBoard();
-    return board[x][y];
-  };
-
-  const getLegalMoves = (x, y, board = makeBoard()) => {
-    for (let i = 0; i < board.length; ++i) {
-      board[i] = board[i].filter(
-        (move) =>
-          move === `[${x - 2}, ${y - 1}]` ||
-          move === `[${x - 1}, ${y - 2}]` ||
-          move === `[${x + 1}, ${y - 2}]` ||
-          move === `[${x + 2}, ${y - 1}]` ||
-          move === `[${x + 2}, ${y + 1}]` ||
-          move === `[${x + 1}, ${y + 2}]` ||
-          move === `[${x - 1}, ${y + 2}]` ||
-          move === `[${x - 2}, ${y + 1}]`
-      );
-    }
-    board = board.flat();
-    return board;
-  };
-
-  const Node = (value) => {
-    return { value };
-  };
-
-  const findRoot = (x, y) => {
-    return Node({ [`[${x}, ${y}]`]: {} });
-  };
-
-  const buildTree = (x, y, target) => {
-    let root = findRoot(x, y);
-    let allMoves = getLegalMoves(x, y);
-    for (let i = 0; i < allMoves.length; ++i) {
-      root.value[`[${x}, ${y}]`][i] = allMoves[i];
-      if (allMoves[i] === target) {
-        return root;
-      }
-    }
-    return root;
-  };
-
-  return { makeBoard, exists, getLegalMoves, buildTree };
+  }
+  return board;
 };
 
-const test = knightMoves();
-console.log(test.makeBoard());
-console.log(test.getLegalMoves(8, 8));
-console.log(test.buildTree(3, 3, "[4, 1]"));
+const q = [];
+const visited = [];
+
+const getLegalMoves = ([x, y], board = makeBoard()) => {
+  for (let i = 0; i < board.length; ++i) {
+    board[i] = board[i].filter(
+      (move) =>
+        move === `[${x - 2}, ${y - 1}]` ||
+        move === `[${x - 1}, ${y - 2}]` ||
+        move === `[${x + 1}, ${y - 2}]` ||
+        move === `[${x + 2}, ${y - 1}]` ||
+        move === `[${x + 2}, ${y + 1}]` ||
+        move === `[${x + 1}, ${y + 2}]` ||
+        move === `[${x - 1}, ${y + 2}]` ||
+        move === `[${x - 2}, ${y + 1}]`
+    );
+  }
+  board = board.flat();
+  return board;
+};
+
+const Node = ([x, y]) => {
+  let node = { value: `[${x}, ${y}]`, prev: null };
+  return node;
+};
+
+const bfs = (node, end) => {
+  visited.push(q.shift());
+  let children = getLegalMoves(JSON.parse(node.value));
+  children.forEach((child) => {
+    child = { value: child, prev: node };
+    if (!visited.some((el) => el.value === child.value)) {
+      q.push(child);
+    }
+  });
+  if (node.value === Node(end).value) {
+    return node;
+  } else {
+    return bfs(q[0], end);
+  }
+  // return node;
+};
+
+const knightMoves = (start, end) => {
+  q.push(Node(start));
+  let path = bfs(q[0], end);
+  return path;
+};
+
+console.log(knightMoves([3, 3], [1, 3]));
